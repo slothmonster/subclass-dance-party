716e6705432dbc4d2059f46dc769b9c992118002
Hack Reactor <hackreactor@Hacks-Mac-F2.local>
2014-03-04 12:41:15 -0800

    added a bunch or functions to dancer.js

--- src/dancer.js
+++ src/dancer.js
@@ -8,9 +8,11 @@ var Dancer = function(top, left, timeBetweenSteps){
   this.$node = $('<span class="dancer"></span>');
 
   this._timeBetweenSteps = timeBetweenSteps;
-  
+  this.chaseMouse = false; 
   this.setPosition(top, left);
   this.step();
+  window.dancers = window.dancers || [];
+  window.dancers.push(this);
 };
 
 Dancer.prototype.step = function(){
@@ -18,6 +20,22 @@ Dancer.prototype.step = function(){
   setTimeout(function(){
     that.step();
   }, this._timeBetweenSteps);
+  if(this.chaseMouse){
+    var posY = parseInt(this.$node.css('top').replace('px','')) + ($(this.$node).height() / 2);
+    var posX = parseInt(this.$node.css('left').replace('px',''))  + ($(this.$node).width() / 2);
+    window.mouseX
+    window.mouseY
+    if(window.mouseX > posX) {
+      this.$node.css('left', (parseInt(this.$node.css('left').replace('px', '')) + 10) + 'px');
+    } else {
+      this.$node.css('left', (parseInt(this.$node.css('left').replace('px', '')) - 10) + 'px');
+    }
+    if(window.mouseY > posY) {
+      this.$node.css('top', (parseInt(this.$node.css('top').replace('px', '')) + 10) + 'px');
+    } else {
+      this.$node.css('top', (parseInt(this.$node.css('top').replace('px', '')) - 10) + 'px');
+    }
+  }
 };
   
 
@@ -28,3 +46,22 @@ Dancer.prototype.setPosition = function(top, left){
   };
   this.$node.css(styleSettings);
 };
+
+Dancer.prototype.lineUp = function(){
+  this.chaseMouse = false;
+  this.$node.attr('data-left', this.$node.css("left"));
+  this.$node.animate({ "left":"150px"}, 1000);
+};
+
+Dancer.prototype.goBack = function() {
+  this.chaseMouse = false;
+  this.$node.animate({ "left": this.$node.attr("data-left")}, 1000);
+}
+
+Dancer.prototype.moveRandomly = function() {
+  this.chaseMouse = false;
+  this.$node.animate({ 
+    "left": ( Math.random() * window.innerWidth ) + "px",
+    "top": ( Math.random() * window.innerHeight ) + "px"
+  }, 1000);
+}
--- src/init.js
+++ src/init.js
@@ -1,5 +1,47 @@
 $(document).ready(function(){
   window.dancers = [];
+  window.sprites = [
+    {
+      "name": "mustache",
+      "height": 128,
+      "width": 128,
+      "url": "images/sprite_mustache.png",
+      interval: 66.6
+    },
+    {
+      "name": "nes",
+      "height": 32,
+      "width": 32,
+      "url": "images/sprite_nes.png",
+      interval: 100
+    },
+    {
+      "name": "doom",
+      "height": 128,
+      "width": 144,
+      "url": "images/sprite_doom.png",
+      interval: 100
+    },
+    {
+      "name": "scorpion",
+      "height": 65,
+      "width": 37,
+      "url": "images/sprite_scorpion.jpeg",
+      interval: 100
+    },
+    {
+      "name": "contra",
+      "height": 46,
+      "width": 38,
+      "url": "images/sprite_contra.png",
+      interval: 100
+    },
+  ];
+
+  $(document).on('mousemove', function(e){
+    window.mouseX = e.clientX;
+    window.mouseY = e.clientY;
+  });
 
   $(".addDancerButton").on("click", function(event){
     /* This function sets up the click handlers for the create-dancer
@@ -26,7 +68,7 @@ $(document).ready(function(){
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
-    window.dancers.push(dancer);
+    // window.dancers.push(dancer);
     $('body').append(dancer.$node);
   });
   $(".addImageDancerButton").on("click", function(event){
@@ -43,7 +85,7 @@ $(document).ready(function(){
         Math.random() * 1000,
         imgUrl
       );
-      window.dancers.push(dancer);
+      // window.dancers.push(dancer);
       $('body').append(dancer.$node);
     });
 
@@ -55,16 +97,42 @@ $(document).ready(function(){
       var dancerMakerFunction = window[dancerMakerFunctionName];
       var top = $("body").height() * Math.random();
       var left = $("body").width() * Math.random();
+      var spriteNumber = Math.floor(Math.random() * window.sprites.length);
       // make a dancer with a random position
       //debugger;
       var dancer = dancerMakerFunction(
         top,
         left,
-        50,
-        imgUrl, 128, 128
+        window.sprites[spriteNumber].interval,
+        window.sprites[spriteNumber].url, window.sprites[spriteNumber].height, window.sprites[spriteNumber].width
       );
-      window.dancers.push(dancer);
+      // window.dancers.push(dancer);
       $('body').append(dancer.$node);
     });  
+  $('.lineup').click(function(){
+    window.dancers.forEach(function(dancer){
+      dancer.lineUp();
+    });
+    $('.chase').attr('checked', false)
+  });
+  $('.goback').click(function(){
+    window.dancers.forEach(function(dancer){
+      dancer.goBack();
+    });
+    $('.chase').attr('checked', false)
+  });
+  $('.move').click(function(){
+    window.dancers.forEach(function(dancer){
+      dancer.moveRandomly();
+    });
+    $('.chase').attr('checked', false)
+  });
+  $('.chase').on('change', function(e){
+    
+    var isChecked = $(this).is(':checked');
+    window.dancers.forEach(function(dancer){
+      dancer.chaseMouse = isChecked;
+    });
+  });
 });
 
